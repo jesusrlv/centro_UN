6385d31f90acc1a2a86f74fd8baead398752760f
Mi nombreRodolfo <jesusrlv.informatica@gmail.com>
2021-05-26 15:53:22 -0500

    tablas

--- trimestres.php
+++ trimestres.php
@@ -133,7 +133,37 @@
       <div class="container-fluid py-5">
         <h1 class="display-5 fw-bold">Sistema de carga de archivos</h1>
         <p class="col-md-8 fs-4">Centro UNESCO | Zacatecas, Zac.</p>
-        <button class="btn btn-primary btn-lg" type="button"><i class="bi bi-file-plus"></i> Carga de archivos</button>
+        <div class="table-responsive">
+            <table class="table table-sm table-bordered table-striped table-hover">
+                <thead class="table-dark">
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
       </div>
     </div>
 </div>
