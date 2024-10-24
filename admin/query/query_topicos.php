7d7b5d4357bec8ceeff0496708f9a387e7277798
jesusrlv <jesusrlvrojo@gmail.com>
2024-10-24 16:30:08 -0600

    avanzando back, los indices no estan correctos

--- admin/query/query_topicos.php
+++ admin/query/query_topicos.php
@@ -123,15 +123,20 @@ else if($topico == 5){
   $resultado_sql = $conn->query($sql);
   while($row_sql = $resultado_sql->fetch_assoc()){
   $catRow = $row_sql['subcategoria'];
+  $catRow2 = $row_sql['categoria'];
   $sqlCat = "SELECT * FROM categoria WHERE id ='$catRow'";
   $resultadoCat = $conn->query($sqlCat);
   $rowCat = $resultadoCat->fetch_assoc();
   
+  $sqlCat2 = "SELECT * FROM topicos WHERE id ='$catRow2'";
+  $resultadoCat2 = $conn->query($sqlCat2);
+  $rowCat2 = $resultadoCat2->fetch_assoc();
+  
     echo'
 
     <div class="col d-flex align-items-start rounded">
-              <div class="icon-square text-dark flex-shrink-0 me-3" style="background-color:#b14947;">
-                <svg class="bi text-light" width="1em" height="1em"><use xlink:href="#money"/></svg>
+              <div class="icon-square text-dark flex-shrink-0 me-3 text-light" style="background-color:#b14947;">
+                <span class="text-light">'.$rowCat2['img'].'</span>
               </div>
               <div>
                 <h2 class="" style="color:#b14947 ;"><strong>'.$rowCat['categoria'].'</strong></h2>
